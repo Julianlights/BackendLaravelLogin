e1d84b85c35f166d8df5462ba2cd95a8b0f095f8
Jorge Luis Julian Sanchez <173211@ids.upchiapas.edu.mx>
2020-06-18 17:04:21 -0500

    Back listo
    Todo el back ya en Git

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Http\Request;
+
+// estas rutas se pueden acceder sin proveer de un token válido.
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+// estas rutas requiren de un token válido para poder accederse.
+Route::group(['middleware' => 'auth.jwt'], function () {
+Route::post('/logout', 'AuthController@logout');
+});
